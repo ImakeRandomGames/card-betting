09e1ffb28854fddedcc770964dfc36f492f72a3b
Hartley Brody <hartley.brody@gmail.com>
2018-03-18 18:12:47 -0400

    first logic, allow player to hit until they bust

--- blackjack.py
+++ blackjack.py
@@ -16,28 +16,58 @@ def deal_cards():
 
     draw_table(dealer_hand, player_hand)
 
+    while True:
+        command = raw_input("What would you like to do? [s]tay, [h]it...")
+
+        if command in ["hit", "h"]:
+            player_hand.append(deck.draw())
+
+            if calculate_total(player_hand) > 21:
+                draw_table(dealer_hand, player_hand)
+                print ""
+                print "   YOU BUSTED  "
+                print
+                return
+
+            else:
+                draw_table(dealer_hand, player_hand)
+                continue
+
+
+        if command in ["stay", "s"]:
+            pass
+
+
 
 
 def draw_table(dealer_hand, player_hand):
     print "========================================"
     print "========================================"
-    print "  Dealer has "
+    print "  Dealer has ", calculate_total(dealer_hand)
     for card in dealer_hand:
         print "    ", card
     print
     print " ~ ~ ~ ~ ~ ~ ~ ~ "
     print
-    print "  Player has "
+    print "  Player has ", calculate_total(player_hand)
     for card in player_hand:
         print "    ", card
     print "========================================"
     print "========================================"
 
-def respond(dealer, player, command):
-    pass
 
 def calculate_total(hand):
-    pass
+    total = 0
+    for card in hand:
+        try:
+            total += int(card.rank)
+        except:
+            if card.rank in ["jack", "queen", "king"]:
+                total += 10
+            elif card.rank == "ace":
+                pass  # TODO
+
+    return total
 
 
 if __name__ == '__main__':
