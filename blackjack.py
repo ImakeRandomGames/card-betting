57e74221d675008c206b40d4f5fb6589700c4fcd
Hartley Brody <hartley.brody@gmail.com>
2018-03-18 21:03:10 -0400

    switch blackjack game to OOP from janky functional style

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,8 @@
 from cards import Deck
 
-deck = Deck()
-
-dealer_hand = []
-player_hand = []
-
-game_mode = None
+#
+# Game End States
+#
 
 class GameOver(Exception):
     pass  # base
@@ -25,113 +22,110 @@ class DealerBusts(PlayerWins):
 class PlayerBusts(DealerWins):
     pass
 
-def deal_cards():
 
-    dealer_card = deck.draw()
-    dealer_card.facing = "down"
-    dealer_hand.append(dealer_card)
+class Game(object):
 
-    player_hand.append(deck.draw())
-    dealer_hand.append(deck.draw())
-    player_hand.append(deck.draw())
+    def __init__(self, mode=None):
+
+        self.deck = Deck()
+        self.mode = mode
+
+        self.dealer_hand = []
+        self.player_hand = []
+
+        # deal out initial cards
+        dealer_card = self.deck.draw()
+        dealer_card.facing = "down"
+
+        self.dealer_hand.append(dealer_card)
+        self.player_hand.append(self.deck.draw())
+        self.dealer_hand.append(self.deck.draw())
+        self.player_hand.append(self.deck.draw())
+
+        if self.mode == "stdin":
+            while True:
+                print self
+                command = raw_input("What would you like to do? [s]tay, [h]it...")
+                self.dealer_command(command)
 
 
-    if game_mode == "stdin":
-        draw_table()
-        while True:
-            command = raw_input("What would you like to do? [s]tay, [h]it...")
-            follow_command(command)
-
-    else:
-        return draw_table()
-
-
-def draw_table(messages=None):
-
-    if game_mode == "stdin":
-        print "========================================"
-        print "========================================"
-        if any([card.facing == "down" for card in dealer_hand]):
-            print "  Dealer has:"
+    def __repr__(self):
+        stdout = "========= A GAME OF BLACKJACK ==========\n"
+        if any([card.facing == "down" for card in self.dealer_hand]):
+            stdout += "  Dealer has:\n"
         else:
-            print "  Dealer has ({}):".format(sum_hand(dealer_hand))
-        for card in dealer_hand:
-            print "    ", card
-        print
-        print " ~ ~ ~ ~ ~ ~ ~ ~ "
-        print
-        print "  Player has ({}):".format(sum_hand(player_hand))
-        for card in player_hand:
-            print "    ", card
-        print "========================================"
-        print "========================================"
+            stdout += "  Dealer has ({}):\n".format(self.sum_hand(self.dealer_hand))
+        for card in self.dealer_hand:
+            stdout += "    {}\n".format(card)
 
-        if messages:
-            print
-            for message in messages:
-                print "   {}    ".format(message.upper())
-            print
+        stdout += " ~ ~ ~ ~ ~ ~ ~ ~ \n"
 
-    else:
-        return {
-            "player_hand": player_hand,
-            "player_sum": sum_hand(player_hand),
-            "dealer_hand": dealer_hand,
-            "dealer_sum": sum_hand(dealer_hand),
-        }
+        stdout += "  Player has ({}):\n".format(self.sum_hand(self.player_hand))
+        for card in self.player_hand:
+            stdout += "    {}\n".format(card)
+        stdout += "========================================\n"
+
+        return stdout
 
 
-def sum_hand(hand):
-    total = 0
-    ace_count = 0
-    for card in hand:
-        try:
-            total += int(card.rank)
-        except:
-            if card.rank in ["jack", "queen", "king"]:
-                total += 10
-            elif card.rank == "ace":
-                ace_count += 1
+    @staticmethod
+    def sum_hand(hand):
+        total = 0
+        ace_count = 0
+        for card in hand:
+            try:
+                total += int(card.rank)
+            except:
+                if card.rank in ["jack", "queen", "king"]:
+                    total += 10
+                elif card.rank == "ace":
+                    ace_count += 1
 
-    # assume aces are 11 unless you'd bust, then 1
-    for x in range(ace_count):
-        if total <= 10:
-            total += 11
-        else:
-            total += 1
+        # assume aces are 11 unless you'd bust, then 1
+        for x in range(ace_count):
+            if total <= 10:
+                total += 11
+            else:
+                total += 1
 
-    return total
+        return total
 
-def follow_command(command):
-    if command in ["hit", "h"]:
-        player_hand.append(deck.draw())
 
-        if sum_hand(player_hand) > 21:
-            raise PlayerBusts()
+    def dealer_command(self, command):
 
-        else:
-            draw_table()
+        if command in ["hit", "h"]:
+            print " => player has chosen to hit"
 
-    if command in ["stay", "s"]:
-        [card.show() for card in dealer_hand]
+            self.player_hand.append(self.deck.draw())
 
-        while sum_hand(dealer_hand) < 17:
-            dealer_hand.append(deck.draw())
+            print self
 
-        draw_table()
-        if sum_hand(dealer_hand) > 21:
-            raise DealerBusts()
+            if self.sum_hand(self.player_hand) > 21:
+                raise PlayerBusts()
 
-        if sum_hand(player_hand) > sum_hand(dealer_hand):
-            raise PlayerWins()
 
-        if sum_hand(player_hand) < sum_hand(dealer_hand):
-            raise DealerWins()
+        if command in ["stay", "s"]:
+            print " => player has chosen to stay"
 
-        if sum_hand(player_hand) == sum_hand(dealer_hand):
-            raise Push()
+            [card.show() for card in self.dealer_hand]
+
+            while self.sum_hand(self.dealer_hand) < 17:
+                self.dealer_hand.append(self.deck.draw())
+
+            print self
+
+            if self.sum_hand(self.dealer_hand) > 21:
+                raise DealerBusts()
+
+            if self.sum_hand(self.player_hand) > self.sum_hand(self.dealer_hand):
+                raise PlayerWins()
+
+            if self.sum_hand(self.player_hand) < self.sum_hand(self.dealer_hand):
+                raise DealerWins()
+
+            if self.sum_hand(self.player_hand) == self.sum_hand(self.dealer_hand):
+                raise Push()
 
 
 if __name__ == '__main__':
-    game_mode = "stdin"
-    deal_cards()
+    Game(mode="stdin")
