a470a7dc89f61566c40ce8577e9c5a86751eb7d8
Hartley Brody <hartley.brody@gmail.com>
2018-03-18 20:08:10 -0400

    broken checkin, issues w game state

--- player.py
+++ player.py
@@ -4,27 +4,61 @@ from blackjack import deal_cards, follow_command, draw_table, PlayerWins, Dealer
 def play():
 
     game = deal_cards()
+    game_result = None
 
     while True:
+
 
         print draw_table()
 
         if game["player_sum"] <= 11:
-            issue_command("hit")
+            game_result = issue_command("hit")
         elif game["player_sum"] >= 17:
-            issue_command("stay")
+            game_result = issue_command("stay")
 
         else:
             for card in game["dealer_hand"]:
                 if card.facing == "up":  # no peeking!
                     if card.rank <= 6:
-                        issue_command("stay")
+                        game_result = issue_command("stay")
                     else:
-                        issue_command("hit")
+                        game_result = issue_command("hit")
+
+        if game_result:
+            break
+
+    return game_result
 
 def issue_command(cmd):
     print "decided to", cmd
-    follow_command(cmd)
+
+    try:
+
+        follow_command(cmd)
+        return None
+
+    except PlayerWins as e:
+        return "win"
+
+    except DealerWins as e:
+        return "lose"
+
+    except Push as e:
+        return "draw"
+
+    return None
 
 if __name__ == '__main__':
-    play()
+
+    for _ in range(100):
+
+        stats = {
+            "win": 0,
+            "lose": 0,
+            "draw": 0,
+        }
+
+        result = play()
+        stats[result] += 1
+
+    print stats
